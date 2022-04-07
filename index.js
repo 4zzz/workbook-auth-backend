f710a3a1b064fcce465a7681690d34205fb6bad2
Matej Mok <matej.mok@gmail.com>
2022-04-07 22:40:29 +0200

    load config from environment variables

--- index.js
+++ index.js
@@ -3,10 +3,31 @@ const http = require('http');
 const https = require('https');
 const axios = require('axios');
 const express = require('express');
-const config = require('./config.json');
 
 const app = express();
-const basePath = config.basePath.endsWith('/') ? config.basePath.slice(0, -1) : config.basePath;
+
+function envOrDef(env, def) {
+  if (process.env[env]) return process.env[env];
+  if (def !== undefined) return def;
+  console.error(`Missing ${env} environment variable`);
+  process.exit(1)
+}
+
+config = {
+  http: {
+    port: envOrDef('HTTP_PORT'),
+  },
+  basePath: envOrDef('BASE_PATH', ''),
+  githubApi: {
+    clientId: envOrDef('CLIENT_ID'),
+    clientSecret: envOrDef('CLIENT_SECRET'),
+  },
+  frontend: {
+    domain: envOrDef('FRONTEND_DOMAIN'),
+    origin: envOrDef('FRONTEND_ORIGIN'),
+    failRedirect: envOrDef('FRONTEND_FAIL_REDIRECT')
+  },
+}
 
 async function getAccessToken(code) {
   const { clientId, clientSecret } = config.githubApi;
@@ -32,7 +53,7 @@ function getLogPrefix(req) {
   return `${req.ip} ${req.method} ${req.path} ${JSON.stringify(req.query)}`;
 }
 
-app.get(`${basePath}/get_access_token`, async (req, res) => {
+app.get(`${config.basePath}/get_access_token`, async (req, res) => {
   const { code, redirect } = req.query;
   if (!code) {
     console.log(`${getLogPrefix(req)}: Invalid request. (Missing code parameter)`);
@@ -40,8 +61,8 @@ app.get(`${basePath}/get_access_token`, async (req, res) => {
   } else {
     try {
       const token = await getAccessToken(code);
-      res.cookie('github_access_token', token);
-      res.redirect(`${config.frontend.hostname}${redirect ? redirect : ''}`);
+      res.cookie('github_access_token', token, {domain: config.frontend.domain, path: '/workbook', sameSite: 'None'});
+      res.redirect(`${config.frontend.origin}${redirect ? redirect : ''}`);
       console.log(`${getLogPrefix(req)}: Authorization successful.`);
     } catch(e) {
       const cause = e.cause ? ` (${e.cause})` : '';
@@ -56,29 +77,7 @@ app.get('*', (req, res) => {
   res.sendStatus(418);
 });
 
-if (config.useHttp) {
-  const { port } = config.http;
-  const httpServer = http.createServer(app);
-  httpServer.listen(port, () => {
-    console.log(`Auth http server listening on port ${port}`)
-  });
-}
-
-if (config.useHttps) {
-  try {
-    var privateKey  = fs.readFileSync(config.https.key, 'utf8');
-    var certificate = fs.readFileSync(config.https.cert, 'utf8');
-    var credentials = {key: privateKey, cert: certificate};
-  } catch (e) {
-    console.error('Failed to load private key file or certificate file');
-    console.error(e);
-    process.exit(1);
-  }
-  if (credentials) {
-    const { port } = config.https;
-    var httpsServer = https.createServer(credentials, app);
-    httpsServer.listen(port, () => {
-      console.log(`Auth https server listening on port ${port}`)
-    });
-  }
-}
+const httpServer = http.createServer(app);
+httpServer.listen(config.http.port, () => {
+  console.log(`Auth http server listening on port ${config.http.port}`)
+});
